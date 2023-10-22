e68294956a5d17ae148988598e0b11d4ff1c9d77
Vladislav Sholokhov <vladislav.sholokhov@jetbrains.com>
2023-10-22 16:41:21 +0300

    intial setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+class Example {
+    constructor(element) {
+        this.element = element;
+    }
+
+    init() {
+        this.element.textContent = 'hello, world!';
+        console.log('ehu!');
+    }
+}
+
+const init = () => {
+    const element = document.getElementById('point');
+    const obj = new Example(element);
+    obj.init();
+}
+
+init();
